4241c1f9a45cb15ea1f73f23617515e55f6a5de9
jodijonatan <jdi102350@gmail.com>
2025-03-06 21:37:18 +0700

    first react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>Testing</nav>
       </header>
+      <section>
+        <h1>HALLO, NAMA AKU JODI JONATAN</h1>
+      </section>
     </div>
   );
 }
