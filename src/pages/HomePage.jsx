6a337e7a6a699a6742f7af1a89271cfd7e9662d1
jodijonatan <jdi102350@gmail.com>
2025-03-09 11:18:47 +0700

    perubahan pada kelas page dan testimoni

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,39 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
 
+import { kelasTerbaru, dataSwiper } from "../data/index";
+import { useNavigate } from "react-router-dom";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
-          <Row className="header-box d-flex align-items-center">
+          <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6">
               <h1 className="mb-4">
-                Temukan <br /> <span>Bakat Kreatifmu</span> <br /> Bersama Kami
+                Junior <br /> <span>Full Stack</span> <br /> Developer
               </h1>
               <p className="mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-                dolorum atque iure quasi quibusdam. Ab.
+                Salah satu siswa Rekayasa Perangkat Lunak dari SMK TELKOM 1
+                MEDAN, nama saya Jodi Jonatan
               </p>
               <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
-                Lihat Kelas
+                About
               </button>
               <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
-                Lihat Promo
+                Contact
               </button>
             </Col>
             <Col lg="6" className="pt-lg-0 pt-5">
@@ -28,7 +42,109 @@ const Home = () => {
           </Row>
         </Container>
       </header>
-      <div className="kelas w-100 min-vh-100"></div>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Kelas Terbaru</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas) => {
+              return (
+                <Col key={kelas.id} className="shadow-sm rounded">
+                  <img
+                    src={kelas.image}
+                    alt="unsplash.com"
+                    className="w-100 mb-5 rounded-top"
+                  />
+                  <div className="star mb-2 px-3">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button
+                className="btn btn-success rounded-5 btn-lg"
+                onClick={() => navigate("/kelas")}
+              >
+                Lihat Semua Kelas{" "}
+                <i className="fa-solid fa-chevron-right ms-1"></i>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold my-5">Testimonial</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
+                992: {
+                  slidesPerView: 2,
+                  spaceBetween: 50,
+                },
+                1200: {
+                  slidesPerView: 3,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {dataSwiper.map((data) => {
+                return (
+                  <SwiperSlide key={data.id} className="shadow-sm">
+                    <p className="desc">{data.desc}</p>
+                    <div className="poeple">
+                      <img src={data.image} alt="" />
+                      <div>
+                        <h5 className="mb-1">{data.name}</h5>
+                        <p className="m-0 fw-bold">{data.skill}</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
--- src/pages/KelasPage.jsx
+++ src/pages/KelasPage.jsx
@@ -1,5 +1,14 @@
 const Kelas = () => {
-  return <div>Kelas</div>;
+  return (
+    <div>
+      <h1>Selamat Bergabung Di Kelas Kami</h1>
+      <p>
+        Anda akan ditemani oleh mentor yang profisional yang sangat ahli di
+        bidangnya
+      </p>
+      <button>Mulai Kelas</button>
+    </div>
+  );
 };
 
 export default Kelas;
