f21f48aded350e31402dc964ae5c31be12206642
jodijonatan <jdi102350@gmail.com>
2025-03-10 15:19:27 +0700

    perubahan pada footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,27 +3,50 @@ import { Link } from "react-router-dom";
 
 const FooterComponent = () => {
   return (
-    <div>
+    <div className="footer py-5">
       <Container>
-        <Row>
-          <Col>
-            <h3>Jo Dev</h3>
-            <p>
+        <Row className="d-flex justify-content-between">
+          <Col lg="5">
+            <h3 className="fw-bold">Jo Dev</h3>
+            <p className="desc">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Recusandae deserunt quasi nesciunt distinctio autem illum dolores
               earum doloribus labore qui?
             </p>
-            <div>
-              <Link>
+            <div className="no mb-1 mt-4">
+              <Link className="text-decoration-none">
                 <i className="fa-brands fa-whatsapp"></i>
-                <p>+62 837-7364-9372</p>
+                <p className="m-0">+62 837-7364-9372</p>
               </Link>
             </div>
-            <div>
-              <Link>
+            <div className="mail">
+              <Link className="text-decoration-none">
                 <i className="fa-regular fa-envelope"></i>
-                <p>sirjo@gmail.com</p>
+                <p className="m-0">sirjo@gmail.com</p>
               </Link>
+            </div>
+          </Col>
+          <Col className="d-flex flex-column col-lg-2 col mt-lg-0 mt-5">
+            <h5 className="fw-bold">Menu</h5>
+            <Link to="">Home</Link>
+            <Link to="kelas">Kelas</Link>
+            <Link to="testimonial">Testimonial</Link>
+            <Link to="foq">FOQ</Link>
+            <Link to="syaratketen">Syarat & Ketentuan</Link>
+          </Col>
+          <Col lg="4" className="mt-lg-0 mt-5">
+            <h5 className="fw-bold mb-3">Subscribe untuk Info Menarik</h5>
+            <div className="subscribes">
+              <input type="text" placeholder="Subscribe..." />
+              <button className="btn btn-danger rounded-end rounded-0">
+                Subscribe
+              </button>
+            </div>
+            <div className="social mt-3">
+              <i className="fa-brands fa-facebook"></i>
+              <i className="fa-brands fa-discord"></i>
+              <i className="fa-brands fa-youtube"></i>
+              <i className="fa-brands fa-linkedin"></i>
             </div>
           </Col>
         </Row>
