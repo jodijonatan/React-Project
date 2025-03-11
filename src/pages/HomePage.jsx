eb047ebabd987ad938dc2638e0347aaf2339c4f7
jodijonatan <jdi102350@gmail.com>
2025-03-11 12:09:16 +0700

    perubahan pada homepage kelas

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,11 +7,9 @@ import FAQComponent from "../components/FAQComponent";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import required modules
 import { Pagination } from "swiper/modules";
 
 const Home = () => {
@@ -49,14 +47,6 @@ const Home = () => {
       <div className="kelas w-100 min-vh-100">
         <Container>
           <Row>
-            <Col>
-              <h1 className="text-center fw-bold">Kelas Terbaru</h1>
-              <p className="text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
-            </Col>
-          </Row>
-          <Row>
             {kelasTerbaru.map((kelas) => {
               return (
                 <Col
@@ -66,25 +56,7 @@ const Home = () => {
                   data-aos-duration="1000"
                   data-aos-delay={kelas.delay}
                 >
-                  <img
-                    src={kelas.image}
-                    alt="unsplash.com"
-                    className="w-100 mb-5 rounded-top"
-                  />
-                  <div className="star mb-2 px-3">
-                    <i className={kelas.star1}></i>
-                    <i className={kelas.star2}></i>
-                    <i className={kelas.star3}></i>
-                    <i className={kelas.star4}></i>
-                    <i className={kelas.star5}></i>
-                  </div>
-                  <h5 className="mb-5 px-3">{kelas.title}</h5>
-                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
-                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
-                    <button className="btn btn-danger rounded-1">
-                      {kelas.buy}
-                    </button>
-                  </div>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3"></div>
                 </Col>
               );
             })}
